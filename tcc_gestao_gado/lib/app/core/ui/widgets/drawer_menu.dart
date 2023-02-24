d3614c0fdd813ad6029353aad28d6b99cf546ad2
Ramos2L <lucasramosleite0@gmail.com>
2023-02-24 20:04:35 -0300

    fix - iniciando home, insercao menu

---
+++ tcc_gestao_gado/lib/app/core/ui/widgets/drawer_menu.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:tcc_gestao_gado/app/core/ui/styles/app_colors.dart';
+import 'package:tcc_gestao_gado/app/core/ui/styles/images.dart';
+import 'package:tcc_gestao_gado/app/core/ui/styles/text_styles.dart';
+import 'package:tcc_gestao_gado/app/core/ui/widgets/circle_avatar_widget.dart';
+
+class DrawerMenu extends StatefulWidget {
+  const DrawerMenu({super.key});
+
+  @override
+  State<DrawerMenu> createState() => _DrawerMenuState();
+}
+
+class _DrawerMenuState extends State<DrawerMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: context.colors.background,
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: context.colors.background,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CircleAvatarWidget(
+                  width: 100,
+                  height: 100,
+                  image: context.images.introImage1,
+                ),
+                Text(
+                  'Lucas Ramos',
+                  style: context.textStyles.textLight.copyWith(
+                    color: context.colors.secondary,
+                    fontSize: 18,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(height: 1.5, color: context.colors.primary),
+          const SizedBox(height: 10),
+          ListTile(
+            title: Text(
+              'Manejo',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Cadastro de animais',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Consultar animais',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Relatório',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Pasto',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Informações',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Conta',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Sobre',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Sair',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.secondary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- tcc_gestao_gado/lib/app/modules/pages/home/home_page.dart
+++ tcc_gestao_gado/lib/app/modules/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:tcc_gestao_gado/app/core/ui/styles/app_colors.dart';
 import 'package:tcc_gestao_gado/app/core/ui/styles/images.dart';
 import 'package:tcc_gestao_gado/app/core/ui/styles/text_styles.dart';
 import 'package:tcc_gestao_gado/app/core/ui/widgets/circle_avatar_widget.dart';
+import 'package:tcc_gestao_gado/app/core/ui/widgets/drawer_menu.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,32 +17,107 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Olá, Lucas',
-          style: context.textStyles.textMedium.copyWith(
-            color: context.colors.primary,
-            fontSize: 18,
-            overflow: TextOverflow.ellipsis,
+        iconTheme: const IconThemeData(color: Colors.white, size: 50),
+        title: SizedBox(
+          height: 50,
+          child: Center(
+            child: Text(
+              'Olá, Lucas',
+              style: context.textStyles.textMedium.copyWith(
+                color: context.colors.onPrimary,
+                fontSize: 20,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
           ),
         ),
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
-          CircleAvatarWidget(
-            width: 55,
-            height: 55,
-            image: context.images.introImage1,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: CircleAvatarWidget(
+              width: 50,
+              height: 50,
+              image: context.images.introImage1,
+            ),
           ),
         ],
-        // leading: CircleAvatarWidget(
-        //   width: 55,
-        //   height: 55,
-        //   image: context.images.introImage1,
-        // ),
       ),
+      drawer: const DrawerMenu(),
+      backgroundColor: context.colors.primary,
       body: SafeArea(
-        child: Container(),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    width: 150,
+                    height: 150,
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    width: 150,
+                    height: 150,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 150,
+                    height: 150,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    width: 150,
+                    height: 150,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.black,
+                    width: 150,
+                    height: 150,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 150,
+                    height: 150,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    color: Colors.pink,
+                    width: 150,
+                    height: 150,
+                  ),
+                  Container(
+                    color: Colors.brown,
+                    width: 150,
+                    height: 150,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
