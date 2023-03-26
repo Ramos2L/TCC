04fbfd800cb7967fcc7c7664e1a9c8a179b512d1
Lucas Ramos <lucas.ramos@enordigital.com>
2023-03-25 21:25:37 -0300

    fix - correcao btn salvar

--- tcc_gestao_gado/lib/app/modules/pages/manejo/descarte/descarte_page.dart
+++ tcc_gestao_gado/lib/app/modules/pages/manejo/descarte/descarte_page.dart
@@ -48,128 +48,148 @@ class _DescartePageState extends State<DescartePage> {
       ),
       drawer: const DrawerMenu(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-            child: Column(
-              children: [
-                const SizedBox(height: 25),
-                Text(
-                  'Descarte do animal',
-                  style: context.textStyles.textMedium.copyWith(
-                    color: context.colors.onPrimary,
-                    fontSize: 20,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Text(
-                  'O descarte do animal ocorre em caso de morte ou venda do mesmo',
-                  style: context.textStyles.textMedium.copyWith(
-                    color: context.colors.onPrimary,
-                    fontSize: 20,
-                  ),
-                ),
-                const SizedBox(height: 25),
-                CustomTextField(
-                  //controller: ,
-                  hintText: 'Data da descarte',
-                  labelStyle: TextStyle(color: context.colors.background),
-                  inputDecoration: InputDecoration(
-                    errorStyle: TextStyle(
-                      fontSize: 14,
-                      color: context.colors.error,
-                    ),
-                  ),
-                  keyboardType: TextInputType.text,
-                  obscureText: false,
-                  suffixIcon: const Icon(Icons.search),
-                  // onFieldSubmitted: (_) {
-                  //   FocusScope.of(context).requestFocus(phoneNode);
-                  // },
-                ),
-                const SizedBox(height: 25),
-                CustomTextField(
-                  //controller: ,
-                  hintText: 'Código do animal',
-                  labelStyle: TextStyle(color: context.colors.background),
-                  inputDecoration: InputDecoration(
-                    errorStyle: TextStyle(
-                      fontSize: 14,
-                      color: context.colors.error,
-                    ),
-                  ),
-                  keyboardType: TextInputType.text,
-                  obscureText: false,
-                  suffixIcon: const Icon(Icons.calendar_month),
-                  // onFieldSubmitted: (_) {
-                  //   FocusScope.of(context).requestFocus(phoneNode);
-                  // },
-                ),
-                const SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      'Situação:',
-                      style: context.textStyles.textMedium.copyWith(
-                        color: context.colors.onPrimary,
-                        fontSize: 20,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Expanded(
-                      child: RadioListTile(
-                        contentPadding: const EdgeInsets.all(0),
-                        title: Text(
-                          'morte',
+        child: CustomScrollView(
+          scrollBehavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate.fixed(
+                [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 25),
+                        Text(
+                          'Descarte do animal',
                           style: context.textStyles.textMedium.copyWith(
                             color: context.colors.onPrimary,
-                            fontSize: 16,
+                            fontSize: 20,
                             overflow: TextOverflow.ellipsis,
                           ),
                         ),
-                        value: "morte",
-                        groupValue: gender,
-                        onChanged: (value) {
-                          setState(() {
-                            gender = value.toString();
-                          });
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: RadioListTile(
-                        contentPadding: const EdgeInsets.all(0),
-                        title: Text(
-                          'venda',
+                        const SizedBox(height: 25),
+                        Text(
+                          'O descarte do animal ocorre em caso de morte ou venda do mesmo',
                           style: context.textStyles.textMedium.copyWith(
                             color: context.colors.onPrimary,
-                            fontSize: 16,
-                            overflow: TextOverflow.ellipsis,
+                            fontSize: 20,
                           ),
                         ),
-                        value: "venda",
-                        groupValue: gender,
-                        onChanged: (value) {
-                          setState(() {
-                            gender = value.toString();
-                          });
-                        },
-                      ),
+                        const SizedBox(height: 25),
+                        CustomTextField(
+                          //controller: ,
+                          hintText: 'Data da descarte',
+                          labelStyle: TextStyle(color: context.colors.background),
+                          inputDecoration: InputDecoration(
+                            errorStyle: TextStyle(
+                              fontSize: 14,
+                              color: context.colors.error,
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: false,
+                          suffixIcon: const Icon(Icons.search),
+                          // onFieldSubmitted: (_) {
+                          //   FocusScope.of(context).requestFocus(phoneNode);
+                          // },
+                        ),
+                        const SizedBox(height: 25),
+                        CustomTextField(
+                          //controller: ,
+                          hintText: 'Código do animal',
+                          labelStyle: TextStyle(color: context.colors.background),
+                          inputDecoration: InputDecoration(
+                            errorStyle: TextStyle(
+                              fontSize: 14,
+                              color: context.colors.error,
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: false,
+                          suffixIcon: const Icon(Icons.calendar_month),
+                          // onFieldSubmitted: (_) {
+                          //   FocusScope.of(context).requestFocus(phoneNode);
+                          // },
+                        ),
+                        const SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              'Situação:',
+                              style: context.textStyles.textMedium.copyWith(
+                                color: context.colors.onPrimary,
+                                fontSize: 20,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Expanded(
+                              child: RadioListTile(
+                                contentPadding: const EdgeInsets.all(0),
+                                title: Text(
+                                  'morte',
+                                  style: context.textStyles.textMedium.copyWith(
+                                    color: context.colors.onPrimary,
+                                    fontSize: 16,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                value: "morte",
+                                groupValue: gender,
+                                onChanged: (value) {
+                                  setState(() {
+                                    gender = value.toString();
+                                  });
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              child: RadioListTile(
+                                contentPadding: const EdgeInsets.all(0),
+                                title: Text(
+                                  'venda',
+                                  style: context.textStyles.textMedium.copyWith(
+                                    color: context.colors.onPrimary,
+                                    fontSize: 16,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                value: "venda",
+                                groupValue: gender,
+                                onChanged: (value) {
+                                  setState(() {
+                                    gender = value.toString();
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                const SizedBox(height: 25),
-                Button.primary(
-                  label: 'SALVAR',
-                  onPressed: () {
-                    //Navigator.pushNamed(context, '/home');
-                  },
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  //const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                    child: Button.primary(
+                      label: 'SALVAR',
+                      onPressed: () {
+                        //Navigator.pushNamed(context, '/home');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
